b7da3c02633ac4d49bb26d30a1aed0719caab5eb
Matheus <ymatheus.h@gmail.com>
2023-11-01 23:22:39 -0300

    style: :lipstick: update tailwind and manifest files

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,15 +2,15 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$$$$1 from "./sections/Theme.tsx";
-import * as $$$$$$2 from "./sections/Footer.tsx";
+import * as $$$$$$0 from "./sections/Theme.tsx";
+import * as $$$$$$1 from "./sections/Footer.tsx";
 import * as $$$$$$$$$$$0 from "./apps/site.ts";
 import * as $$$$$$$$$$$1 from "./apps/decohub.ts";
 
 const manifest = {
   "sections": {
-    "deco-sites/start/sections/Footer.tsx": $$$$$$2,
-    "deco-sites/start/sections/Theme.tsx": $$$$$$1,
+    "deco-sites/start/sections/Footer.tsx": $$$$$$1,
+    "deco-sites/start/sections/Theme.tsx": $$$$$$0,
   },
   "apps": {
     "deco-sites/start/apps/decohub.ts": $$$$$$$$$$$1,
