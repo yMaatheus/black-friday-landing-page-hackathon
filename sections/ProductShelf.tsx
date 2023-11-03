1bd77b00d36196e219de1e98f9222ba222993626
Matheus <ymatheus.h@gmail.com>
2023-11-03 13:13:20 -0300

    feat: :sparkles: create sections product shelf and tabbed product shelf

---
+++ sections/ProductShelf.tsx
@@ -0,0 +1,73 @@
+import Icon from "$store/components/ui/Icon.tsx";
+import Header from "$store/components/ui/SectionHeader.tsx";
+import Slider from "$store/components/ui/Slider.tsx";
+import SliderJS from "$store/islands/SliderJS.tsx";
+import { useId } from "$store/sdk/useId.ts";
+import type { Product } from "apps/commerce/types.ts";
+import ProductCard from "$store/components/ProductCard/index.tsx";
+
+export interface Props {
+  products: Product[] | null;
+  title?: string;
+  description?: string;
+  layout?: {
+    headerAlignment?: "center" | "left";
+    headerfontSize?: "Normal" | "Large";
+  };
+}
+
+export default function ProductShelf({
+  products,
+  title,
+  description,
+  layout,
+}: Props) {
+  const id = useId();
+
+  if (!products || products.length === 0) {
+    return null;
+  }
+
+  return (
+    <div class="w-full container  py-8 flex flex-col gap-12 lg:gap-16 lg:py-10">
+      <Header
+        title={title || ""}
+        description={description || ""}
+        fontSize={layout?.headerfontSize || "Large"}
+        alignment={layout?.headerAlignment || "center"}
+      />
+
+      <div
+        id={id}
+        class="container grid grid-cols-[48px_1fr_48px] px-0 sm:px-5"
+      >
+        <Slider class="carousel carousel-center sm:carousel-end gap-6 col-span-full row-start-2 row-end-5">
+          {products?.map((product, index) => (
+            <Slider.Item
+              index={index}
+              class="carousel-item w-[270px] sm:w-[292px] first:pl-6 sm:first:pl-0 last:pr-6 sm:last:pr-0"
+            >
+              <ProductCard
+                product={product}
+              />
+            </Slider.Item>
+          ))}
+        </Slider>
+
+        <>
+          <div class="hidden relative sm:block z-10 col-start-1 row-start-3">
+            <Slider.PrevButton class="btn btn-circle btn-outline absolute right-1/2 bg-base-100">
+              <Icon size={24} id="ChevronLeft" strokeWidth={3} />
+            </Slider.PrevButton>
+          </div>
+          <div class="hidden relative sm:block z-10 col-start-3 row-start-3">
+            <Slider.NextButton class="btn btn-circle btn-outline absolute left-1/2 bg-base-100">
+              <Icon size={24} id="ChevronRight" strokeWidth={3} />
+            </Slider.NextButton>
+          </div>
+        </>
+        <SliderJS rootId={id} />
+      </div>
+    </div>
+  );
+}
